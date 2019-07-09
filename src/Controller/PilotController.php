0cc3655bc87d47a87d9faf95f49255df66db55ea
Brian Dendauw <brian.dendauw@student.howest.be>
2019-07-09 12:51:20 +0200

    setup read routing pilot

--- src/Controller/PilotController.php
+++ src/Controller/PilotController.php
@@ -13,8 +13,22 @@ class PilotController extends AbstractController
      */
     public function index()
     {
+        $pilot = array(
+            "id" => 1,
+            "firstname" => "bobby",
+            "lastname" => "bobson",
+            "rank" => "Private"
+        );
+        $pilot2 = array(
+            "id" => 2,
+            "firstname" => "billy",
+            "lastname" => "billson",
+            "rank" => "Private"
+        );
+
         $pilots = array(
-            "pilot1" => "bobby"
+            "pilot1" => $pilot,
+            "pilot2" => $pilot2
         );
 
         return $this->render('pilots/index.html.twig', [
@@ -28,6 +42,16 @@ class PilotController extends AbstractController
     public function create()
     {
         return $this->render('pilots/create.html.twig');
+    }
+
+    /**
+     * @Route("/pilot/{id}");
+     */
+    public function read(int $id)
+    {
+        return $this->render('pilots/details.html.twig', [
+            'id' => $id
+        ]);
     }
 
 }
