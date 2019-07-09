0851b2a06d5f49a740e560205d74c7bd6870f126
Brian Dendauw <brian.dendauw@student.howest.be>
2019-07-09 14:27:25 +0200

    start creating a plane

--- src/Controller/PlaneController.php
+++ src/Controller/PlaneController.php
@@ -6,12 +6,13 @@ namespace App\Controller;
 use App\Entity\Plane;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PlaneController extends AbstractController
 {
     /**
-     * @Route("/planes");
+     * @Route("/planes", name="planes");
      */
     public function index()
     {
@@ -22,5 +23,29 @@ class PlaneController extends AbstractController
         ]);
     }
 
-    
+    /**
+     * @Route("/create-plane", name="create-plane");
+     */
+    public function create(Request $request)
+    {
+        $plane = new Plane();
+
+        $form = $this->createFormBuilder($plane)->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plane = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($plane);
+            $em->flush();
+
+            return $this->redirectToRoute('planes');
+        }
+
+        return $this->render('planes/create.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
 }
