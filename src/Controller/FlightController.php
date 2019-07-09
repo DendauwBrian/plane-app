38a309c55757ee5123e2f7801c7bc75e4feec7fd
Brian Dendauw <brian.dendauw@student.howest.be>
2019-07-09 15:33:51 +0200

    setup initial flight routing paths

--- src/Controller/FlightController.php
+++ src/Controller/FlightController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FlightController extends AbstractController
@@ -14,5 +15,21 @@ class FlightController extends AbstractController
     public function index()
     {
         return $this->render('flights/index.html.twig');
+    }
+
+    /**
+     * @Route("/flight/new", name="newflight");
+     */
+    public function create(Request $request)
+    {
+        return $this->render("flights/create.html.twig");
+    }
+
+    /**
+     * @Route("/flight/history", name="historyOfFlights");
+     */
+    public function history()
+    {
+        return $this->render("flights/history.html.twig");
     }
 }
