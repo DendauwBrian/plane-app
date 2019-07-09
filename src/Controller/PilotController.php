005a9ea69b47fc0466015d8118254bfd9f8e3902
Brian Dendauw <brian.dendauw@student.howest.be>
2019-07-09 15:19:45 +0200

    edit a pilot

--- src/Controller/PilotController.php
+++ src/Controller/PilotController.php
@@ -84,6 +84,46 @@ class PilotController extends AbstractController
     }
 
     /**
+     * @Route("/pilot/edit/{id}", name="edit-pilot");
+     */
+    public function edit(Request $request, int $id)
+    {
+        $pilot = new Pilot();
+        $pilot = $this->getDoctrine()->getRepository(Pilot::class)->find($id);
+
+        $form = $this->createFormBuilder($pilot)
+            ->add("firstname", TextType::class, array('attr' => array("class" => "form-control")))
+            ->add("lastname", TextType::class, array('attr' => array("class" => "form-control")))
+            ->add("age", IntegerType::class, array('attr' => array("class" => "form-control", "min" => 18, "max" => 65)))
+            ->add("rank", ChoiceType::class, array('choices' => array(
+                "Private" => "Private",
+                "Flight Engineer" => "Flight Engineer",
+                "Second Officer" => "Second Officer",
+                "First Officer" => "First Officer",
+                "Captain" => "Captain"
+            ), 'attr' => array("class" => "form-control")))
+            ->add("retired", ChoiceType::class, array('choices' => array(
+                "False" => false,
+                "True" => true
+            ), 'attr' => array("class" => "form-control")))
+            ->add("save", SubmitType::class, array('label' => 'Save', 'attr' => array("class" => "btn btn-primary")))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('pilots');
+        }
+
+        return $this->render('pilots/edit.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
      * @Route("/pilot/{id}", name="single-pilot");
      */
     public function details(int $id)
