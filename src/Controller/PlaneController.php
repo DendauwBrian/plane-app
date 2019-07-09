429bf1a614ea9076a7c5fb3fc40a36ac92ea818c
Brian Dendauw <brian.dendauw@student.howest.be>
2019-07-09 14:39:55 +0200

    setup create a plane

--- src/Controller/PlaneController.php
+++ src/Controller/PlaneController.php
@@ -6,6 +6,10 @@ namespace App\Controller;
 use App\Entity\Plane;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,12 +34,26 @@ class PlaneController extends AbstractController
     {
         $plane = new Plane();
 
-        $form = $this->createFormBuilder($plane)->getForm();
+        $form = $this->createFormBuilder($plane)
+            ->add("model", TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add("manufacturer", TextType::class, array('attr' => array("class" => "form-control")))
+            ->add("engines", ChoiceType::class, array('choices' => array(
+                "Wankel" => "Wankel",
+                "Diesel" => "Diesel",
+                "Jet" => "Jet",
+                "Electric" => "Electric"
+            ), 'attr' => array("class" => "form-control")))
+            ->add("save", SubmitType::class, array('label' => 'Create', 'attr' => array("class" => "btn btn-primary")))
+            ->getForm();
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $plane = $form->getData();
+
+            $dateTime = new \DateTime('@'.strtotime('now'));
+
+            $plane->setBuildDay($dateTime);
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($plane);
