1eb91519f270c0ad663babbb352a89aa7a6e4aa8
Brian Dendauw <brian.dendauw@student.howest.be>
2019-07-09 11:51:44 +0200

    initial db entities

---
+++ src/Entity/Pilot.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PilotRepository")
+ */
+class Pilot
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $rank;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $retired;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getRank(): ?string
+    {
+        return $this->rank;
+    }
+
+    public function setRank(string $rank): self
+    {
+        $this->rank = $rank;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getRetired(): ?bool
+    {
+        return $this->retired;
+    }
+
+    public function setRetired(bool $retired): self
+    {
+        $this->retired = $retired;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/Plane.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PlaneRepository")
+ */
+class Plane
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Manufacturer;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Engines;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $firstflight;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $lastflight;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getManufacturer(): ?string
+    {
+        return $this->Manufacturer;
+    }
+
+    public function setManufacturer(string $Manufacturer): self
+    {
+        $this->Manufacturer = $Manufacturer;
+
+        return $this;
+    }
+
+    public function getEngines(): ?string
+    {
+        return $this->Engines;
+    }
+
+    public function setEngines(?string $Engines): self
+    {
+        $this->Engines = $Engines;
+
+        return $this;
+    }
+
+    public function getFirstflight(): ?\DateTimeInterface
+    {
+        return $this->firstflight;
+    }
+
+    public function setFirstflight(\DateTimeInterface $firstflight): self
+    {
+        $this->firstflight = $firstflight;
+
+        return $this;
+    }
+
+    public function getLastflight(): ?\DateTimeInterface
+    {
+        return $this->lastflight;
+    }
+
+    public function setLastflight(?\DateTimeInterface $lastflight): self
+    {
+        $this->lastflight = $lastflight;
+
+        return $this;
+    }
+}
